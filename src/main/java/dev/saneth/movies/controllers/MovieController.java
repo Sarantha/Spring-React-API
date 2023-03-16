91f0e2b3a613a09a1b36297e263b6bc71f6a28fc
Sarantha <ssmahadoowage@gmail.com>
2023-03-16 21:55:22 +0530

    add review controller and review repository

--- src/main/java/dev/saneth/movies/controllers/MovieController.java
+++ src/main/java/dev/saneth/movies/controllers/MovieController.java
@@ -2,14 +2,17 @@ package dev.saneth.movies.controllers;
 
 import dev.saneth.movies.entities.Movie;
 import dev.saneth.movies.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -26,5 +29,11 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies(){
         List<Movie> movieList = movieService.allMovies();
         return new ResponseEntity<List<Movie>>(movieList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String id){
+        Optional<Movie> movie = movieService.selectedMovie(id);
+        return new ResponseEntity<Optional<Movie>>(movie,HttpStatus.OK);
     }
 }
